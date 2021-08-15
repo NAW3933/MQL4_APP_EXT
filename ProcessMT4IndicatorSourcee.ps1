c37f7726d84e408e18a8f29e5c5f683d955df247
NAW3933 <50742075+NAW3933@users.noreply.github.com>
2021-08-15 22:20:13 +0100

    Added beginning of power shell scripts to process bulk of MT4 files

---
+++ ProcessMT4IndicatorSourcee.ps1
@@ -0,0 +1,37 @@
+﻿$Depth = 3
+$Root = 'C:\forexcollection\2020'
+$Targe
+
+$Levels = '/*' * 2
+$DirObjects=Get-ChildItem -Directory $Root/$Levels
+ForEach ($SubDir in ($DirObjects | ?{$_.PSIsContainer})){
+
+    $ObjectCount=Get-ChildItem $SubDir -Recurse | Measure-Object | %{$_.Cou}
+    
+    $FolderCount = Get-ChildItem $SubDir -Recurse -Directory | Measure-Object | %{$_.Count}
+    
+    $FileCount= Get-ChildItem $SubDir -Recurse -File | Measure-Object | %{$_.Count}
+    
+    $Mql4Files =  Get-ChildItem $SubDir -Filter *.mql4| Measure-Object | %{$_.Count}
+    
+    $ex4Files = Get-ChildItem $SubDir -Filter *.ex4| Measure-Object | %{$_.Count}
+    
+    if(($FileCount=1) -And ($FolderCount=1))
+    {
+        ''
+        $SubDir.Name
+        '-----------------------------------------------------------------------------'
+        Get-ChildItem -Path $SubDir.FullName -Recurse | Where-Object {$_.PSIsContainer -eq $false}  
+    }
+
+
+    <#
+    if (-not(Test-Path -Path $SubDir/Indicator)){  
+        $SubDir.FullName
+    } else {
+        #"Path doesn't exist."
+    }
+    #>
+    
+}
+
