88c69a9451b2c00a6500bbb52f74113d23703ade
NAW3933 <50742075+NAW3933@users.noreply.github.com>
2021-08-16 19:39:05 +0100

    Modified Code

--- ProcessMT4IndicatorSourcee.ps1
+++ ProcessMT4IndicatorSourcee.ps1
@@ -1,29 +1,41 @@
 ﻿
-
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 $Depth = 3
 $Root = 'C:\forexcollection\2020'
-$Targe
+$TrgtRt = ''
+$MQLFilesNum= 0
+$EX4FileNum=0
 
 $Levels = '/*' * 2
 $DirObjects=Get-ChildItem -Directory $Root/$Levels
 ForEach ($SubDir in ($DirObjects | ?{$_.PSIsContainer})){
 
-    $ObjectCount=Get-ChildItem $SubDir -Recurse | Measure-Object | %{$_.Cou}
+    $ObjectCount = Get-ChildItem $SubDir -Recurse | Measure-Object | %{$_.Count}
+    $FolderCount = Get-ChildItem $SubDir -Recurse -Directory | Measure-Object| %{$_.Count}
+    $FilesCount = Get-ChildItem $SubDir -Recurse -File | Measure-Object| %{$_.Count}
     
-    $FolderCount = Get-ChildItem $SubDir -Recurse -Directory | Measure-Object | %{$_.Count}
-    
-    $FileCount= Get-ChildItem $SubDir -Recurse -File | Measure-Object | %{$_.Count}
-    
-    $Mql4Files =  Get-ChildItem $SubDir -Filter *.mql4| Measure-Object | %{$_.Count}
-    
-    $ex4Files = Get-ChildItem $SubDir -Filter *.ex4| Measure-Object | %{$_.Count}
-    
-    if(($FileCount.ToDecimal()=1) -And ($FolderCount=1))
+    $MQLFilesNum= Get-ChildItem $SubDir -Recurse -File -Filter *.MQ4| Measure-Object| %{$_.Count}
+    $EX4FileNum = Get-ChildItem $SubDir -Recurse -File -Filter *.MQ4| Measure-Object| %{$_.Count}
+
+
+    if(($FilesCount -eq 2) -And ($FolderCount -eq 0))
     {
+ 
+        $FolderCount
+        $FileCount
+        
         ''
         $SubDir.Name
-        '-----------------------------------------------------------------------------'
-        Get-ChildItem -Path $SubDir.FullName -Recurse | Where-Object {$_.PSIsContainer -eq $false}  
+        #'-----------------------------------------------------------------------------'
+        #Get-ChildItem -Path $SubDir.FullName -Recurse | Where-Object {$_.PSIsContainer -eq $false}  
+        #$MQLFilesNum.ToString() + ' MQL files'
+        #$EX4FileNum.ToString() + ' EX4 files'
+        
+        if($MQLFilesNum -eq 1 -and $EX4FileNum -eq 1)
+        {
+            'HERE'
+            Get-ChildItem -Path $SubDir.FullName -Recurse | Where-Object {$_.PSIsContainer -eq $false}
+        }
     }
 
 
