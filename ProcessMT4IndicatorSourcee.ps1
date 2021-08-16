2287fc3af29f145d23c419b8b692d991ca118917
NAW3933 <50742075+NAW3933@users.noreply.github.com>
2021-08-16 20:47:10 +0100

    Corrected Script - just prmissions to set

--- ProcessMT4IndicatorSourcee.ps1
+++ ProcessMT4IndicatorSourcee.ps1
@@ -2,7 +2,7 @@
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 $Depth = 3
 $Root = 'C:\forexcollection\2020'
-$TrgtRt = ''
+$TrgtRt = 'C:\forexcollection\temp\Indicators'
 $MQLFilesNum= 0
 $EX4FileNum=0
 
@@ -15,29 +15,41 @@ ForEach ($SubDir in ($DirObjects | ?{$_.PSIsContainer})){
     $FilesCount = Get-ChildItem $SubDir -Recurse -File | Measure-Object| %{$_.Count}
     
     $MQLFilesNum= Get-ChildItem $SubDir -Recurse -File -Filter *.MQ4| Measure-Object| %{$_.Count}
-    $EX4FileNum = Get-ChildItem $SubDir -Recurse -File -Filter *.MQ4| Measure-Object| %{$_.Count}
+    $EX4FileNum = Get-ChildItem $SubDir -Recurse -File -Filter *.EX4| Measure-Object| %{$_.Count}
 
 
-    if(($FilesCount -eq 2) -And ($FolderCount -eq 0))
+    if(($FilesCount -gt 0) -And ($FolderCount -eq 0))
     {
  
-        $FolderCount
-        $FileCount
+        #$FolderCount
+        #$FileCount
         
-        ''
-        $SubDir.Name
+        #''
+        #$SubDir.Name
         #'-----------------------------------------------------------------------------'
         #Get-ChildItem -Path $SubDir.FullName -Recurse | Where-Object {$_.PSIsContainer -eq $false}  
         #$MQLFilesNum.ToString() + ' MQL files'
         #$EX4FileNum.ToString() + ' EX4 files'
         
-        if($MQLFilesNum -eq 1 -and $EX4FileNum -eq 1)
+        if($MQLFilesNum -eq $EX4FileNum)
         {
-            'HERE'
-            Get-ChildItem -Path $SubDir.FullName -Recurse | Where-Object {$_.PSIsContainer -eq $false}
-        }
-    }
+            Get-ChildItem -Filter "*.mq4"|Foreach-Object{
+                   Remove-Item $_.BaseName+".ex4";
+                   Move-Item -Path $_.FullName -Destination $TrgtRt
+            }
 
+            #Get-ChildItem -Filter "*.mq4"|Foreach-Object{
+            #       Move-Item -Path $($_.FullName) -Destination $dstDir
+            #}            
+        }
+
+    }
+    if(($FilesCount -gt 0) -And ($FolderCount -eq 0))
+    {
+            Get-ChildItem |Foreach-Object{
+                   Move-Item -Path $_.FullName -Destination $TrgtRt
+            }    
+    }
 
     <#
     if (-not(Test-Path -Path $SubDir/Indicator)){  
